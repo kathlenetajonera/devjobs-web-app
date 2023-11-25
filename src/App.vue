285abde17c6e3425e7897f008c652c0e0b3483e5
Kathlene Tajonera <kathlene.tajonera@gmail.com>
2023-11-25 20:16:49 +0800

    feat: added router scroll behavior and attribution

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import Attribution from './components/Attribution.vue';
 </script>
 
 <template>
     <Header />
     <router-view />
+    <Attribution />
 </template>
---
+++ src/components/Attribution.vue
@@ -0,0 +1,13 @@
+<template>
+    <p class="py-4 text-black text-xs text-center bg-light-gray dark:bg-dark dark:text-white">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" class="underline">
+            Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://github.com/kathlenetajonera" class="underline" target="_blank">
+            Kathlene&nbsp;Tajonera
+        </a>
+        .
+    </p>
+</template>
